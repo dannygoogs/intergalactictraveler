3971ff8c6ebd3af2d88d46c96e8aeb07b02bb9e6
daniel gougler <dannyg10503@icloud.com>
2020-10-21 09:14:29 -0400

    Initial Commit

---
+++ intergalactictraveler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  intergalactictraveler
+//
+//  Created by Daniel Gougler on 10/21/20.
+//  Copyright © 2020 goog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
